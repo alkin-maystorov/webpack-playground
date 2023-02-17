7befefc6548a5bf7b1da8e54af1ffd6a8bf4c086
Alkin <a.maystorov@vehiculum.de>
2023-02-17 17:23:13 +0100

    add webpack prod and dev envs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 const App = () => {
   return (
     <>
-      <h1 className='text-lg text-purple-400'>Webpack Playground</h1>
+      <h1 className='text-lg text-purple-400'>
+        Webpack Playground - {`${process.env.NODE_ENV}`} {`${process.env.name}`}
+      </h1>
       <figure className='w-[300px] h-[300px]'>
         <img src='https://res.cloudinary.com/vehiculum/image/upload/v1553701978/cars/2010-nissan/2010-nissan-370z-r-design-convertible/2010-nissan-370z-r-design-convertible-front-view.png' />
       </figure>
