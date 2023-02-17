9c0444c7022578d12a6b359d32c5ce8e9d75b6b0
Alkin <a.maystorov@vehiculum.de>
2023-02-17 16:55:50 +0100

    add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 const App = () => {
-  return <h1>Webpack Playground</h1>;
+  return (
+    <>
+      <h1 className='text-lg text-purple-400'>Webpack Playground</h1>
+      <figure className='w-[300px] h-[300px]'>
+        <img src='https://res.cloudinary.com/vehiculum/image/upload/v1553701978/cars/2010-nissan/2010-nissan-370z-r-design-convertible/2010-nissan-370z-r-design-convertible-front-view.png' />
+      </figure>
+    </>
+  );
 };
 
 export default App;
